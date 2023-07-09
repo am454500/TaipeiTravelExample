845cbd359af98b6794a23805f991f50787d269ff
PaulZ <am454500@gmail.com>
2023-07-09 23:10:09 +0800

    moshi bug修改

--- app/src/main/java/com/example/taipeitravel/api/ApiClient.kt
+++ app/src/main/java/com/example/taipeitravel/api/ApiClient.kt
@@ -1,24 +1,12 @@
 package com.example.taipeitravel.api
 
 import com.example.taipeitravel.BuildConfig
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
-
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
-var gson: Gson = GsonBuilder()
-    .setLenient()
-    .create()
 
 
 /**
@@ -43,7 +31,7 @@ class ApiClient {
         .build()
 
     private val retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addConverterFactory(MoshiConverterFactory.create())
         .client(okHttpClient)
         .baseUrl(
             HttpUrl.Builder().scheme("https")
